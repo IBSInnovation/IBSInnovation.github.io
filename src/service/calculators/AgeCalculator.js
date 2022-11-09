b03ab08d30e35b949a2be49ed30b9d3ea93aefa8
PjotrOoms <pjotto99@hotmail.nl>
2022-11-09 21:01:05 +0100

    Fixed age bug

--- src/service/calculators/AgeCalculator.js
+++ src/service/calculators/AgeCalculator.js
@@ -1,11 +1,10 @@
 export function formatBirthDateToAge(dateString) {
-  let from = dateString.split("-");
-  let birthdateTimeStamp = new Date(from[2], from[1] - 1, from[0]);
-  let cur = new Date();
-  let diff = cur - birthdateTimeStamp;
+  let birthdateTimeStamp = new Date(dateString);
+  let currentDate = new Date();
   // This is the difference in milliseconds
-  let currentAge = Math.floor(diff / 31557600000);
+  let diff = currentDate - birthdateTimeStamp;
   // Divide by 1000*60*60*24*365.25
+  let currentAge = Math.floor(diff / 31557600000);
 
   return currentAge;
 }
