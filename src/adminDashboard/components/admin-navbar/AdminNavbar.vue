4b6a20dd114023fda5f3cb171c9d8cdd64e8fe4a
gianni <xlightningclawx110402@gmail.com>
2023-03-15 13:21:44 +0100

    added navbar content

--- src/adminDashboard/components/admin-navbar/AdminNavbar.vue
+++ src/adminDashboard/components/admin-navbar/AdminNavbar.vue
@@ -1,21 +1,50 @@
 <template>
   <div class="sidebar">
     <h2>Admin Dashboard</h2>
-  
-    <NavbarLink to="/">Home</NavbarLink>
-  
+    <h3> {{ getDisplayName() }} </h3>
+
+    <div class="navitems">
+      <ul>
+        <li>
+          <a role="menuitem" tabindex="0" @click="goToPatients()" @keyup.enter="goToPatients()">Home</a>
+        </li>
+        <li>
+          <a role="menuitem" tabindex="0" @click="goToUserManage()" @keyup.enter="goToUserManage()">User Management </a>
+        </li>
+        <li>
+          <a role="menuitem" tabindex="0">Test</a>
+        </li>
+      </ul>
+    </div>
+
   </div>
 </template>
   
 <script>
-import NavbarLink from "./AdminNavbarLink.vue"
 
 export default {
   name: "AdminNavbar",
-  components: {
-    NavbarLink
+  methods: {
+    goToUserManage() {
+      this.$router.push({ name: "user" });
+    },
+    goToPatients() {
+      this.$router.push({ name: "patients" });
+    },
+    getDisplayName() {
+      // if registered with mail  -> mail is the displayName
+      let email = this.$store.getters.getUser.email;
+      let displayName = this.$store.getters.getUser.displayName;
+      if (typeof displayName === "undefined") {
+        return email;
+      } else {
+        return displayName;
+      }
+    }
   }
-  }
+}
+
+
 </script>
 
 <style>
@@ -39,5 +68,29 @@ export default {
 
   display: flex;
   flex-direction: column;
+}
+
+.sidebar h2 {
+  text-align: center;
+  border-style: groove hidden ridge;
+  border-width: 2px;
+  border-color: grey;
+  
+}
+
+.sidebar h3 {
+  text-align: center;
+}
+
+.navitems li {
+  padding: 10px;
+
+  border: 10px;
+  border-color: red;
+  border-radius: 20px;
+}
+
+.navitems a {
+  cursor: pointer;
 }
 </style>
