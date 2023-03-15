566a7503f024107e34c95e84fa30056ff3c9172c
gianni <xlightningclawx110402@gmail.com>
2023-03-15 12:14:36 +0100

    added functional code to navbar

---
+++ src/adminDashboard/components/admin-navbar/AdminNavbar.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="sidebar">
+    <h2>Admin Dashboard</h2>
+  
+    <NavbarLink to="/">Home</NavbarLink>
+  
+  </div>
+</template>
+  
+<script>
+import NavbarLink from "./AdminNavbarLink.vue"
+
+export default {
+  name: "AdminNavbar",
+  components: {
+    NavbarLink
+  }
+  }
+</script>
+
+<style>
+:root {
+  --sidebar-bg-color: #2f855a;
+}
+</style>
+
+<style scoped>
+.sidebar {
+  color: white;
+  background-color: var(--sidebar-bg-color);
+
+  float: left;
+  position: fixed;
+  z-index: 1;
+  top: 0px;
+  left: 0;
+  bottom: 0;
+  padding: 0.5em;
+
+  display: flex;
+  flex-direction: column;
+}
+</style>
