6ebd5831a2d03c294b4c659004852f2791e4359e
gianni <xlightningclawx110402@gmail.com>
2023-03-15 12:14:57 +0100

    added content to usermanage

--- src/adminDashboard/components/navbar/Navbar.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div class="container">
-        
-    </div>
-
-</template>
--- src/adminDashboard/views/UserManage.vue
+++ src/adminDashboard/views/UserManage.vue
@@ -1,5 +1,19 @@
+<script>
+import AdminNavbar from '../components/admin-navbar/AdminNavbar.vue';
+
+export default {
+  name: "PatientInfo",
+  components: {
+    AdminNavbar
+  }
+}
+
+</script>
+
 <template>
+    <AdminNavbar></AdminNavbar>
     <div class="container">
         <h1>Hello World!</h1>
+        <h2>u wab</h2>
     </div>
 </template>
