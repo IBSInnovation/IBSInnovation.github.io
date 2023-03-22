9d22fec86e5a90f640be6d8f815171cec3aac765
GianniGr <GianniGr@users.noreply.github.com>
2023-03-22 13:06:36 +0000

    deploy: a548c7ed3c7ea076ee59a5b270da5b4fc8305352

---
+++ assets/BackButton.3ab474f4.js
@@ -0,0 +1,1 @@
+import{_ as e,o as s,c,p as n,f as a,d as _}from"./index.d630a810.js";const p={name:"BackButton"},r=t=>(n("data-v-05617b49"),t=t(),a(),t),d=r(()=>_("b",null,"Terug",-1)),u=[d];function B(t,o,i,l,k,b){return s(),c("button",{class:"backBtn",onClick:o[0]||(o[0]=f=>t.$router.go(-1))},u)}const m=e(p,[["render",B],["__scopeId","data-v-05617b49"]]);export{m as B};
