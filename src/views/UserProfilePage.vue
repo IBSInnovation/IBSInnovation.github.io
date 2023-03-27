7ef5fb288900395df106043e0d89e10541d1ec2e
GianniG <xlightningclawx110402@gmail.com>
2023-03-27 10:44:57 +0200

    created profile page setup

---
+++ src/views/UserProfilePage.vue
@@ -0,0 +1,33 @@
+<template>
+    <nav-bar-top></nav-bar-top>
+  
+    <footer><BackButton></BackButton></footer>
+  </template>
+  
+  <script>
+  import NavBarTop from "../components/navigation/NavBarTop.vue";
+  import { useRoute } from "vue-router";
+  import BackButton from "../components/buttons/BackButton.vue";
+  
+  export default {
+    name: "UserProfilePage",
+    components: {
+      NavBarTop,
+      BackButton,
+    },
+    data() {
+      return {
+        route: useRoute()
+      };
+    },
+  
+    methods: {
+    },
+  };
+  </script>
+  
+  <style scoped>
+
+
+  </style>
+  
