7d73cadc3012e9b6223d51c51b6cc114d6f4f437
gianni <xlightningclawx110402@gmail.com>
2023-04-12 16:09:08 +0200

    css applied to children

--- src/adminDashboard/views/CreateExercise.vue
+++ src/adminDashboard/views/CreateExercise.vue
@@ -15,13 +15,20 @@
         <textarea rows="2" cols="23" required placeholder="Description..."></textarea>
       </div>
 
-      <div className="input-field" id="input-group">
-        <input type="text" required placeholder="Step" />
+      <div className="steps-header">
+        <h3>Add Steps</h3>
+        <a className="addInput" href="#" @click.prevent="addInput()">&plus;</a>
       </div>
-      <a className="addInput" href="#" @click.prevent="addInput()">&plus;</a>
 
-      <div>
-        <button className="createbtn" type="submit"> Create </button>
+      <div id="input-group">
+        <div className="flex input-field">
+          <input type="text" required placeholder="Step" />
+          <a className="btn" type="button"> X </a>
+        </div>
+      </div>
+
+      <div className="actionButtons">
+        <button className="btn" type="submit"> Create </button>
       </div>
 
     </form>
@@ -39,14 +46,23 @@ export default {
       const step = document.createElement("input");
       step.type = "text"
       step.placeholder = "Step"
-      step.required
+      step.required = true;
 
-      // const btn = document.createElement("a");
-      // btn.innerHTML = "Remove"
-      // btn.className = "delete"
+      const btn = document.createElement("button");
+      btn.innerHTML = "X"
+      btn.type = "button"
+      btn.classList.add("btn")
+      btn.onclick = function () {
+        this.parentElement.remove();
+      }
 
-      inputDivSelector.appendChild(step);
-      inputDivSelector.appendChild(btn);
+      const flexDiv = document.createElement("div");
+      flexDiv.classList.add("flex");
+      flexDiv.classList.add("input-field");
+
+      inputDivSelector.appendChild(flexDiv);
+      flexDiv.appendChild(step);
+      flexDiv.appendChild(btn);
     },
   }
 }
@@ -57,40 +73,51 @@ export default {
   position: relative;
   text-align: center;
   background: #323232;
-  width: 20%;
+  width: 22rem;
   border-radius: 20px;
   align-items: center;
   justify-content: center;
   box-shadow: 0 3px 5px rgba(0, 0, 0, 0.3);
-  padding: 60px 40px;
+  padding: 20px;
 }
 
-.input-field {
+:deep(.input-field) {
   width: 100%;
   padding-bottom: 15px;
   outline: none;
   font-size: 19px;
-  transition: .4s;
 }
 
-.createbtn,
-.delete {
+:deep(.btn) {
+  margin-top: 5px;
+  margin-bottom: 5px;
   justify-content: center;
   border-radius: 18px;
   background-color: #e43a23;
-  padding-top: 0.5em;
-  padding-bottom: 0.5em;
+  padding-top: 0.3em;
+  padding-bottom: 0.3em;
   color: white;
   border: none;
   margin-left: auto;
   width: 100px;
 }
 
-.createbtn,
-.delete:hover,
-.createbtn,
-.delete:focus {
+:deep(.btn:hover,
+.btn:focus) {
   background: #d3322c;
+}
+
+.steps-header {
+  justify-content: space-between;
+  display: flex;
+  border-bottom: 2px solid #fff;
+  margin-bottom: 10px;
+}
+
+:deep(.flex) {
+  display: flex;
+  gap: 0.5em;
+  justify-content: space-between;
 }
 
 .addInput {
@@ -98,11 +125,10 @@ export default {
   height: 24px;
   width: 24px;
   display: inline-block;
-  background-color: gray;
+  background-color: #e43a23;
   border-radius: 18px;
   color: white;
   font-size: 24px;
   line-height: 24px;
-  margin-bottom: 15px;
 }
 </style>
